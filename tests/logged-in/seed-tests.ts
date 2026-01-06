a2275b2500e2618a45aae86bebec3c0fe2667a49
Samantha Sheets <Samantha.Sheets@galenhealthcare.com>
2026-01-06 10:31:39 -0700

    add playwright config for testMatch to determine seed file naming requirements

---
+++ tests/logged-in/seed-tests.ts
@@ -0,0 +1,13 @@
+// since testMatch: "**/*tests.ts" in playwright.config.ts, this file was renamed from seed.spec.ts
+
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import { expect } from '@playwright/test';
+import { addMovie, createList, openLists, addImageToList, navigateToMovieList } from '../helpers/list-utilities';
+import { listTest as test } from '../helpers/list-test';
+
+
+test.describe('Test group', () => {
+  test('seed', async ({ listPage }) => {
+    const page = listPage; //set the page to the list page fixture
+  });
+});
